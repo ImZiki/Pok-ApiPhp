79783a8629b1189a6567f766a0d7664a09db18c6
Ziki <98941902+ImZiki@users.noreply.github.com>
2025-01-18 17:48:38 +0100

    Update header.php

--- src/header.php
+++ src/header.php
@@ -1,3 +1,9 @@
+<?php
+// Evitar múltiples llamadas a session_start()
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <header>
     <div class="logo">
         <img src="../assets/img/logo.png" alt="PokéAPI Logo">
@@ -5,15 +11,12 @@
     </div>
     <nav>
         <?php
-            session_start(); // Iniciar la sesión para verificar el estado del usuario
-            if (isset($_SESSION['username'])) {
-                // Si el usuario está logeado, mostramos el enlace de "Logout"
-                echo '<a href="logout.php">Logout</a>';
-            } else {
-                // Si el usuario no está logeado, mostramos los enlaces de "Login" y "Registro"
-                echo '<a href="login.php">Login</a>';
-                echo '<a href="register.php">Sign up</a>';
-            }
+        if (isset($_SESSION['username'])) {
+            echo '<a href="logout.php">Logout</a>';
+        } else {
+            echo '<a href="login.php">Login</a>';
+            echo '<a href="register.php">Sign up</a>';
+        }
         ?>
     </nav>
 </header>
