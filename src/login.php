df658670d4c4893427f0d33e3ca39c9e157b6469
Ziki <98941902+ImZiki@users.noreply.github.com>
2025-01-18 17:49:08 +0100

    Update login.php

--- src/login.php
+++ src/login.php
@@ -1,3 +1,21 @@
+<?php
+// Iniciar sesión antes de cualquier salida
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+require_once './utils/auth.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username-input'];
+    $password = $_POST['password-input'];
+    if (login($username, $password)) {
+        header("location: api.php");
+        exit();
+    } else {
+        $_SESSION['error'] = "Credenciales incorrectas. Inténtalo de nuevo.";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,41 +25,18 @@
     <title>Login</title>
 </head>
 <body>
-<?php require_once 'header.php';?>
-<?php
-// Iniciar sesión si aún no se ha iniciado
-require_once './utils/auth.php';
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username-input'];
-    $password = $_POST['password-input'];
-    if (login($username, $password)) {
-        header("location: api.php");
-        exit();
-    } else {
-        $_SESSION['error'] = "Credenciales incorrectas. Inténtalo de nuevo."; // Establecer mensaje de error
-    }
-}
-?>
+<?php require_once 'header.php'; ?>
 <div class="login-container">
     <form action="login.php" method="POST" class="login-form">
         <h2>Login</h2>
-
-        <!-- Mostrar mensaje de error si está presente en la sesión -->
         <?php if (isset($_SESSION['error'])): ?>
             <p class="error-message"><?php echo $_SESSION['error']; ?></p>
-            <?php unset($_SESSION['error']); // Eliminar el mensaje después de mostrarlo ?>
+            <?php unset($_SESSION['error']); ?>
         <?php endif; ?>
-
         <label for="username-input" class="form-label">Username</label>
         <input type="text" name="username-input" class="form-input" placeholder="Enter your username" required>
-
         <label for="password-input" class="form-label">Password</label>
         <input type="password" name="password-input" class="form-input" placeholder="Enter your password" required>
-
         <input type="submit" value="Login" class="form-submit">
     </form>
 </div>
