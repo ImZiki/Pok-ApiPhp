80e614547052d117cae45a042eb6c0d3124596b4
Ziki <siklrp@gmail.com>
2025-01-20 19:22:39 +0100

    fix again

--- src/header.php
+++ src/header.php
@@ -1,11 +1,18 @@
+<?php
+$page = basename($_SERVER['PHP_SELF']);
+?>
+
 <header>
     <div class="logo">
         <img src="../assets/img/logo.png" alt="PokéAPI Logo">
         <h1>PokéAPI</h1>
     </div>
-    <nav id="nav-links">
-        <!-- Los enlaces serán cargados dinámicamente aquí -->
+    <nav>
+        <?php if ($page === 'api.php'): ?>
+            <a href="logout.php">Logout</a>
+        <?php else: ?>
+            <a href="login.php">Login</a>
+            <a href="register.php">Sign up</a>
+        <?php endif; ?>
     </nav>
 </header>
-
-<script src="header.js"></script>
